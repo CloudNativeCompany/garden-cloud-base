835f7b361fb10a44207f84307e0ce130209f8ee7
tony-is-coding <newbiwtan@163.com>
2022-06-08 22:54:43 +0800

    just commit it 0

--- garden-cloud-starter/garden-cloud-starter-sentinel/src/main/java/org/cnc/garden/cloud/sentinel/config/SentinelCustomAutoConfiguration.java
+++ garden-cloud-starter/garden-cloud-starter-sentinel/src/main/java/org/cnc/garden/cloud/sentinel/config/SentinelCustomAutoConfiguration.java
@@ -3,11 +3,13 @@ package org.cnc.garden.cloud.sentinel.config;
 import com.alibaba.cloud.sentinel.feign.SentinelFeignAutoConfiguration;
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
 import com.alibaba.fastjson.JSONObject;
+import org.cnc.garden.cloud.common.factory.YamlPropertySourceFactory;
 import org.cnc.garden.cloud.common.result.Response;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,8 +21,9 @@ import javax.servlet.http.HttpServletRequest;
  * @date 2022/6/6 16:50
  */
 @Configuration(proxyBeanMethods= false)
+@PropertySource(factory = YamlPropertySourceFactory.class, value ="classpath:sentinel-conf.yml")
 @AutoConfigureBefore(SentinelFeignAutoConfiguration.class)
-public class SentinelAutoConfiguration {
+public class SentinelCustomAutoConfiguration {
 
     /**
      * 限流、熔断统一处理类
